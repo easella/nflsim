cf532c316740c54d9a028b11e66fb3e4f16b5ad7
easella <74786054+easella@users.noreply.github.com>
2022-05-24 11:53:31 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
+
+
 const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
   "/",
-  "/nfl.jpeg",
   "/index.html",
- "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css""
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "/nfl.png"
 ];
 
 self.addEventListener("install", installEvent => {
