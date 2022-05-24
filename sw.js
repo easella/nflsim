5aa90f066b2cb421da697be2dfefb84cf378e532
easella <74786054+easella@users.noreply.github.com>
2022-05-23 21:40:10 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener('install', (e)=>{e.waitUntil( caches.open('fox-store').then((cache)=> cache.addAll([ "/","/sw.js","https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"])), );});self.addEventListener('fetch', (e)=>{console.log(e.request.url); e.respondWith( caches.match(e.request).then((response)=> response || fetch(e.request)), );});
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+ "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css""
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
